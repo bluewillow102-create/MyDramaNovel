1248ad4c557e171a3ac7913024bba9b74b2e3509
bluewillow102-create <bluewillow102@gmail.com>
2026-08-31 02:03:30 +0800

    Add mydramanovel.lua for custom extensions

---
+++ mydramanovel.lua
@@ -0,0 +1,12 @@
+{
+  "name": "My Custom Extensions",
+  "author": "Community",
+  "extensions": [
+    {
+      "id": "mydramanovel",
+      "name": "MyDramaNovel",
+      "version": 1,
+      "file": "mydramanovel.lua"
+    }
+  ]
+}
